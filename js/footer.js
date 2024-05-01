3de728f5d15c125fa37cf3aa332635190b67e58c
aamirkhan829055 <aamirkhanhnd12@gmail.com>
2024-05-01 17:31:38 +0530

    new web

---
+++ js/footer.js
@@ -0,0 +1,145 @@
+if (document.getElementById("footer")) {
+    document.getElementById("footer").innerHTML = ` 
+    <footer class="home-footer-1">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+                        <div class="footer-logo text-center">
+                            <img src="images/footer-logo.png" alt="" class="">
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-6 col-12">
+                        <div class="list-a-11">
+                            <a href="#" class=""><strong>Pages</strong></a>
+
+                        </div>
+
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-6 col-12">
+                        <div class="list-a-11">
+                            <a href="#" class=""><strong>Utility</strong></a>
+                        </div>
+
+                    </div>
+
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+                        <div class="list-a-11 ">
+                            <a href="#" class=""><strong>Subscribe</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+                    <div class="Transport-Services border-0 business-text pt-4">
+                    <img src="images/footer-logo.png" alt="" class="pb-3 display-none-text">
+                        <p class="text-white Leverage">Leverage agile frameworks to provide a robust synopsis for
+                            strategy
+                            collaborative thinking to
+                            further the overall value
+                            proposition.</p>
+                    </div>
+
+                    <div>
+
+
+                        <div class="d-flex align-items-center mt-4">
+                            <div class="header-top-icon  d-flex justify-content-center align-items-center">
+                                <img src="images/Group 2487.svg">
+                            </div>
+                            <div class="Sunday-Closed pl-3">
+                                <p class="m-0 text-white">Email</p>
+                                <p class="m-0 text-white">contact@logistics.com</p>
+
+                            </div>
+
+                        </div>
+
+                        <div class="d-flex align-items-center mt-4">
+                            <div class="header-top-icon d-flex justify-content-center align-items-center ">
+                                <img src="images/Vector.svg">
+                            </div>
+
+                            <div class="Sunday-Closed pl-3">
+                                <p class="m-0 text-white">Call Us</p>
+                                <p class="m-0 text-white">(00) 112 365 489</p>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-2 col-sm-6 col-12">
+                <div class="list-a-11 display-none-text">
+                            <a href="#" class=""><strong>Pages</strong></a>
+
+                        </div>
+                    <ul class="list-a-11 pt-4">
+                        <li><a href="#" class=" text-white">About Us
+                            </a></li>
+                        <li><a href="#" class=" text-white">Our Team
+                            </a></li>
+                        <li><a href="#" class=" text-white">Our Project</a></li>
+                        <li><a href="#" class=" text-white">Pricing</a></li>
+                        <li><a href="#" class=" text-white">Contact</a></li>
+                    </ul>
+
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-2 col-sm-6 col-12">
+                <div class="list-a-11 display-none-text">
+                            <a href="#" class=""><strong>Utility</strong></a>
+                        </div>
+                    <ul class="list-a-11 pt-4">
+                        <li><a href="#" class="text-white">Style Guide</a></li>
+                        <li><a href="#" class="text-white">Changelog</a></li>
+                        <li><a href="#" class="text-white">Licenses</a></li>
+                        <li><a href="#" class="text-white">Protected</a></li>
+                        <li><a href="#" class="text-white">Not Found</a></li>
+                    </ul>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+                <div class="list-a-11 display-none-text">
+                            <a href="#" class=""><strong>Subscribe</strong></a>
+                        </div>
+                    <div class="footer-input pt-4">
+                        <input type="text" class="py-3 pl-3" placeholder="Email here*">
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <div class="Chain-Solutions-1 mt-3">
+                            <button class="button-tab border-0">Send Now</button>
+                        </div>
+                        <div class="d-flex">
+                            <i class="fa-brands fa-linkedin text-white pl-4"></i>
+
+                            <i class="fa-brands fa-twitter text-white pl-4"></i><i
+                                class="fa-brands fa-facebook text-white pl-4"></i>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <div class=" border-top-99 mt-4">
+            <div class="container">
+                <div class="d-flex justify-content-between pt-4">
+                    <p class="text-white transhfollw">Copyright © TransitFlow | Designed by VictorFlow - Powered by
+                        Webflow.
+                    </p>
+                    <ul class="footer-list-ul-li">
+
+                        <li class="d-inline-block"><a href="#" class=" text-white">Style Guide
+                            </a></li>
+                        <li class="d-inline-block"><a href=" #" class=" text-white pl-3">Licenses</a></li>
+                        <li class="d-inline-block"><a href=" #" class=" text-white pl-3">Changelog</a></li>
+                        <li class="d-inline-block"><a href=" #" class=" text-white pl-3">Password</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer> `;
+}
---
+++ js/header.js
@@ -0,0 +1,110 @@
+if (document.getElementById("header")) {
+    document.getElementById("header").innerHTML = ` 
+       <header>
+        <div class="header-nabvar py-4">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center header-nabvar-1">
+                    <div class="logo-svg-first">
+                        <img src="images/Logo.svg" alt="" class="">
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <div class="header-top-icon d-flex justify-content-center align-items-center">
+                                <img src="images/Group 2486.svg">
+                            </div>
+
+                            <div class="Sunday-Closed pl-3">
+                                <p class="m-0 text-white">Mon - Sat 9.00 - 18.00</p>
+                                <p class="m-0 text-white">Sunday Closed</p>
+
+                            </div>
+                        </div>
+
+                        <div class="d-flex align-items-center ml-4">
+                            <div class="header-top-icon  d-flex justify-content-center align-items-center">
+                                <img src="images/Group 2487.svg">
+                            </div>
+
+                            <div class="Sunday-Closed pl-3">
+                                <p class="m-0 text-white">Email</p>
+                                <p class="m-0 text-white">contact@logistics.com</p>
+
+                            </div>
+
+                        </div>
+
+                        <div class="d-flex align-items-center ml-4">
+                            <div class="header-top-icon d-flex justify-content-center align-items-center ">
+                                <img src="images/Vector.svg">
+                            </div>
+
+                            <div class="Sunday-Closed pl-3">
+                                <p class="m-0 text-white">Call Us</p>
+                                <p class="m-0 text-white">(00) 112 365 489</p>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="true-header">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light p-0">
+                    <a class="navbar-brand" href="#">
+                        <div class="logo-svg">
+                            <img src="images/Logo.svg" alt="" class="">
+                        </div>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active list-a" target="_self">
+                                <a class="nav-link text-white list-a-1 border-0" href="#">Home</a>
+                            </li>
+                            <li class="nav-item list-a">
+                                <a class="nav-link text-white list-a-1" href="#">About</a>
+                            </li>
+                            <li class="nav-item dropdown list-a">
+                                <a class="nav-link dropdown-toggle text-white list-a-1" href="#" id="navbarDropdown"
+                                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Pages
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+
+                            <li class="nav-item list-a">
+                                <a class="nav-link text-white list-a-1" href="#">Project</a>
+                            </li>
+                            <li class="nav-item list-a">
+                                <a class="nav-link text-white list-a-1" href="#">contact</a>
+                            </li>
+                        </ul>
+
+                        <div class="social-icon">
+                            <i class="fa-brands fa-instagram text-white"></i>
+                            <i class="fa-brands fa-facebook text-white"></i>
+                            <i class="fa-brands fa-twitter text-white"></i>
+                            <i class="fa-brands fa-linkedin mr-5 text-white"></i>
+                            <button class=" text-center border-0 bg-white" title="Request-burron" type="text">Request
+                                Quote</button>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+    
+    `;
+}
